77b170ce47069c7da56b013368c7eb181ae02ef5
RoeyApel <roiapel123@gmail.com>
2024-09-26 11:22:03 +0300

    g

--- Java (Mario)/Exercise6_Bingo/src/BingoBoard.java
+++ Java (Mario)/Exercise6_Bingo/src/BingoBoard.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class BingoBoard {
     int[][] board;
     int[] notInBoard;
@@ -9,7 +7,7 @@ public class BingoBoard {
         notInBoard[0] = deck[deck.length - 2];
         notInBoard[1] = deck[deck.length - 1];
         board = new int[7][];
-                
+
         for (int i = 0; i < board.length; i++) {
             board[i] = new int[i + 1];
         }
