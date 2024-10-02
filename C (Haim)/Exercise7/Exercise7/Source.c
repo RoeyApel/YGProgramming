b3774a608e76757c982742bcae32250db906ed64
RoeyApel <roiapel123@gmail.com>
2024-10-02 09:39:19 +0300

    d

--- C (Haim)/Exercise7/Exercise7/Source.c
+++ C (Haim)/Exercise7/Exercise7/Source.c
@@ -1,11 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define N 10
 
 #include <stdio.h>
 
 void shiftToStart(int* arr, int size, int shifts);
 
 void main() {
+	int arr[N] = { 1,2,3,4,5,6,7,8,9,10 };
+	shiftToStart(arr, N, 5);
 
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d ", *(arr + i));
+	}
 }
 
 void shiftToStart(int* arr, int size, int shifts) {
@@ -25,7 +32,7 @@ void shiftToStart(int* arr, int size, int shifts) {
 	int k;
 	for (k = 0, i = size - shifts; i < size; k++, i++)
 	{
-		*(arr + i) = *(arr + k);
+		*(arr + i) = *(sarr + k);
 	}
 }
 
