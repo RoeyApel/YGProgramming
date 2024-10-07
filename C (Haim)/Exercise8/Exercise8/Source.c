2d0f4f710aa122b1d11c89401e60320325b309e0
RoeyApel <roiapel123@gmail.com>
2024-10-07 16:32:34 +0300

    t

--- C (Haim)/Exercise8/Exercise8/Source.c
+++ C (Haim)/Exercise8/Exercise8/Source.c
@@ -2,10 +2,66 @@
 
 #include <stdio.h>
 
+#define N 20
+
 int bSearch(int* arr, int size, int num);
+void swap(int* a, int* b);
+void marge(int* pa, int sizeA, int* pb, int sizeB, int* pc);
+void bubleSortByIndex(int** arr, int size);
+void getArrInput(char* arr, int size);
 
 void main() {
+	char a[N];
+	int ch;
 
+	getArrInput(a, N);
+	/*for (int i = 0; i < N; i++)
+	{
+		putch(*(a + i));
+	}*/
+
+	//int* arr[N], a[N] = { 1,2,4,5,3 };
+
+	//for (int i = 0; i < N; i++)
+	//{
+	//	*(arr + i) = (a + i);
+	//}
+
+	//bubleSortByIndex(arr, N);
+
+	//for (int i = 0; i < N; i++)
+	//{
+	//	printf("%d\n", **(arr + i));
+	//}
+}
+
+void getArrInput(char* arr, int size) {
+	char ch;
+	for (int i = 0; i < size; i++)
+	{
+		//ch = getch();
+
+		*(arr + i) = getch();
+	}
+}
+
+void bubleSortByIndex(int** arr, int size) {
+	int i, j;
+	for (i = size - 1; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (**(arr + j) > **(arr + j + 1)) {
+				swap((arr + j), (arr + j + 1));
+			}
+		}
+	}
+}
+
+void swap(int* a, int* b) {
+	*a += *b;
+	*b = *a - *b;
+	*a -= *b;
 }
 
 void marge(int* pa, int sizeA, int* pb, int sizeB, int* pc) {
