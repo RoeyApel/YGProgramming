c25f3571fd3c589cb958080794fdcb69d8093f03
RoeyApel <roiapel123@gmail.com>
2024-10-09 10:16:31 +0300

    f

--- C (Haim)/Exercise8/Exercise8/Source.c
+++ C (Haim)/Exercise8/Exercise8/Source.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <conio.h>
+#include<ctype.h>
 
 #define N 20
 
@@ -13,6 +14,7 @@ void bubleSortByIndex(int** arr, int size);
 void getArrInput(char* arr, int size);
 void printArr(char* arr, int size);
 void changeArr(char* arr, int size);
+void changeArr2(char* arr, int size);
 
 void main() {
 	char arr[N];
@@ -24,6 +26,36 @@ void main() {
 	printArr(arr, N);
 }
 
+int intParse() {
+
+}
+
+void changeArr2(char* arr, int size) {
+	char ch;
+
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		ch = *(arr + i);
+		if (isupper(ch)) {
+			ch = tolower(ch);
+		}
+		else if (islower(ch)) {
+			ch = toupper(ch);
+		}
+		else if (isdigit(ch))
+		{
+			if (ch == '9') {
+				ch = '0';
+			}
+			else {
+				ch++;
+			}
+		}
+		*(arr + i) = ch;
+	}
+}
+
 void changeArr(char* arr, int size) {
 	char ch;
 
@@ -32,16 +64,16 @@ void changeArr(char* arr, int size) {
 	{
 		ch = *(arr + i);
 		if (ch >= 'A' && ch <= 'Z') {
-			ch += 32;
+			ch += 'a' - 'A';
 		}
 		else if (ch >= 'a' && ch <= 'z') {
-			ch -= 32;
+			ch -= 'a' - 'A';
 		}
-		else if(ch >= '0' && ch <= '8')
+		else if (ch >= '0' && ch <= '8')
 		{
 			ch++;
 		}
-		else if(ch == '9')
+		else if (ch == '9')
 		{
 			ch = '0';
 		}
