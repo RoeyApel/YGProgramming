8814f09b7d6546e4337e980f610599fd9324ac65
RoeyApel <roiapel123@gmail.com>
2024-10-29 07:51:53 +0200

    h

---
+++ C (Haim)/Exercise11/Exercise11/Source.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char* str);
+void swapCh(char* a, char* b);
+
+void main() {
+
+}
+
+void reverse(char* str) {
+	int i, len = strlen(str);
+	for (i = 0; i < len / 2; i++)
+	{
+		swapCh(str + i, str + len - 1 - i);
+	}
+}
+
+void swapCh(char* a, char* b) {
+	*a += *b;
+	*b = *a - *b;
+	*a -= *b;
+}
