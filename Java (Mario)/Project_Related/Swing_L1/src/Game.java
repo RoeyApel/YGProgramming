b4b3452b7ca8c9def5a6971e0bb57ce37947abef
RoeyApel <roiapel123@gmail.com>
2024-11-03 16:08:03 +0200

    m

--- Java (Mario)/Project_Related/Swing_L1/src/Game.java
+++ Java (Mario)/Project_Related/Swing_L1/src/Game.java
@@ -12,7 +12,7 @@ public class Game {
 
     public void initComponents() {
         button = new CustomButton(gameFrame.CENTER_X - 50, gameFrame.CENTER_Y - 25, 100, 50, "Kill me");
-        button.setLocation(3,3);
+        button.setBounds(3, 3, button.getWidth(), button.getHeight());
     }
 
     public void addComponentsToFrame() {
