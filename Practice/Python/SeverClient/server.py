3ef321eed87b3d2f9bcb46e2bf53d33417465646
RoeyApel <roiapel123@gmail.com>
2024-11-28 12:24:19 +0200

    p

--- Practice/Python/SeverClient/server.py
+++ Practice/Python/SeverClient/server.py
@@ -3,4 +3,25 @@ import threading
 
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def start():
+    server.listen(5)
+
+    while True:
+        conn, addr = server.accept()
+        print(f"connected to {addr}")
+
+        msg = conn.recv(2048).decode('utf-8')
+        print(f"message from client: {msg}")
+
+        if msg != "exit":
+            conn.send("message received!")
+        else:
+            server.close()
+            break
+
 
