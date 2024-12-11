72d1a15e6c38a795f193e148448d78866eddb01e
RoeyApel <roiapel123@gmail.com>
2024-12-11 14:21:29 +0200

    f

--- Java (Mario)/Project_Related/Damaca/src/Pieces.java
+++ Java (Mario)/Project_Related/Damaca/src/Pieces.java
@@ -1,18 +1,19 @@
 import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
 
 public enum Pieces {
-    PLAYER("DAMACA/images/checker1.svg", 60),
-    OPPONENT("DAMACA/images/checker2.svg", 60),
-    PLAYER_KING("DAMACA/images/checker1.svg", 70),
-    OPPONENT_KING("DAMACA/images/checker1.svg", 70);
+    PLAYER("images/checker1.svg", 60),
+    OPPONENT("images/checker2.svg", 60),
+    PLAYER_KING("images/checker1.svg", 70),
+    OPPONENT_KING("images/checker1.svg", 70);
 
     private Image image;
     private int size;
 
     Pieces(String path, int size) {
-        image = new ImageIcon(path).getImage();
+        image = new ImageIcon(getClass().getResource(path)).getImage();
         image.getScaledInstance(size, size, Image.SCALE_SMOOTH);
         this.size = size;
     }
