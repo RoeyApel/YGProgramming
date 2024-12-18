00976ebc3dedfddb7b1539d61641f2f96bb73432
RoeyApel <roiapel123@gmail.com>
2024-12-18 21:57:11 +0200

    h

--- C (Haim)/Exercise26/Exercise26/Source.c
+++ C (Haim)/Exercise26/Exercise26/Source.c
@@ -21,12 +21,134 @@ typedef struct nodeType {
 	struct nodeType* next;
 } nodeType, * nodePtr;
 
+typedef struct {
+	int start;
+	int finish;
+}dataType2;
+
+typedef struct nodeType2 {
+	dataType2 data;
+	struct nodeType2* next;
+} nodeType2, * nodePtr2;
+
 // functions declerations
 int func(nodePtr list);
 void* getPositive(nodePtr* head);
 void flip(nodePtr* list);
+void deleteValue(nodePtr* list, int num);
+int findBigValue(nodePtr list);
+void moveSmallNodeFirst(nodePtr* list);
+nodePtr split(nodePtr* list);
+nodePtr2 selection(nodePtr2* list);
 
 void main() {
+}
+
+nodePtr2 selection(nodePtr2* list) {
+	if (!(*list)) {
+		puts("The list is empty idiot");
+	}
+
+	nodePtr2 temp = *list, newList = NULL, moved;
+
+	while (temp->next) {
+		if (temp->data.finish > temp->next->data.start) {
+			moved = temp->next;
+			temp->next = moved->next;
+			moved->next = newList;
+			newList = moved;
+		}
+		else {
+			temp = temp->next;
+		}
+	}
+	return newList;	
+}
+
+nodePtr split(nodePtr* list) {
+	if (!(*list)) {
+		puts("stop no please no the list is empty as your brain");
+		return;
+	}
+
+	nodePtr list2 = NULL, temp, moved;
+	
+	if ((*list)->data.num < 0) {
+		moved = (*list);
+		*list = (*list)->next;
+		moved->next = list2;
+		list2 = moved;
+	}
+
+	for (temp = *list; temp->next; temp = temp->next) {
+		if (temp->next->data.num < 0) {
+			moved = temp->next;
+			temp->next = moved->next;
+			moved->next = list2;
+			list2 = moved;
+		}
+	}
+	return list2;
+}
+
+void moveSmallNodeFirst(nodePtr* list) {
+	if (!(*list)) {
+		puts("stop no please no the list is empty as your brain");
+		return;
+	}
+
+	nodePtr temp, beforeSmall,small;
+	int min = (*list)->data.num;
+
+	for (temp = *list; temp->next; temp = temp->next) {
+		if (temp->next->data.num < min) {
+			min = temp->next->data.num;
+			beforeSmall = temp;
+		}
+	}
+	if ((*list)->data.num == min) {
+		return;
+	}
+	small = beforeSmall->next; 
+	beforeSmall->next = small->next;
+	small->next = *list;
+	*list = small;
+}
+
+void deleteValue(nodePtr* list, int num) {
+	if (!(*list)) {
+		return;
+	}
+	nodePtr temp, deleted;
+
+	if ((*list)->data.num == num) {
+		deleted = *list;
+		*list = (*list)->next;
+		free(deleted);
+	}
+
+	for (temp = *list; temp->next; temp = temp->next) {
+		if (temp->next->data.num == num) {
+			deleted = temp->next; 
+			temp = temp->next->next;
+			free(deleted);
+		}
+	}
+}
+
+int findBigValue(nodePtr list) {
+	if (!list) {
+		return -1;
+	}
+	nodePtr temp; 
+	int max = list->data.num; 
+	
+	for (temp = list->next; temp; temp = temp->next) {
+		if (temp->data.num > max) {
+			max = temp->data.num;
+		}
+	}
+	return max;
 }
 
 int func(nodePtr list) {
---
+++ SQL (Khali)/Exercise14.sql
@@ -0,0 +1,54 @@
+create schema classes;
+
+create table students (
+student_id int primary key,
+first_name varchar(30) not null, 
+last_name varchar(30) not null, 
+city varchar(40)
+);
+
+create table courses (
+course_id int primary key, 
+course_name varchar(30) not null, 
+points int 
+);
+
+create table grades (
+student_id int,
+course_id int, 
+grade int,
+
+primary key (student_id, course_id),
+
+constraint fk_student
+foreign key (student_id) references students(student_id),
+
+constraint fk_course
+foreign key (course_id) references courses(course_id)
+);
+
+create table good_grades (
+student_id int,
+course_id int,
+grade int
+);
+
+insert into students 
+values (1,"Tom", "Popy","city1"),
+(2,"Tim", "Popy","city2"),
+(3,"Tum", "Popy","city3");
+
+insert into courses 
+values (1,"Sql", 20),
+(2,"Java", 20),
+(3,"Mario",60);
+
+insert into grades 
+values(1,2,80),
+(2,3,60),
+(2,2,90),
+(1,2,100);
+
+insert into good_grades 
+select student_id,course_id,grade from grades
+where grade >= 80;
