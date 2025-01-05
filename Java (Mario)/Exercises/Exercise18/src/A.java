39a0f24121725f91acd48834bcfb4775690be1b9
RoeyApel <roiapel123@gmail.com>
2025-01-05 22:42:24 +0200

    Update A.java

--- Java (Mario)/Exercises/Exercise18/src/A.java
+++ Java (Mario)/Exercises/Exercise18/src/A.java
@@ -1,5 +1,5 @@
-public class A implements B{
-    public static void aaa(){
+public class A implements B {
+    public static void aaa() {
         System.out.println("kill me");
         System.err.println(B.sum(3));
         Object b = new Object();
