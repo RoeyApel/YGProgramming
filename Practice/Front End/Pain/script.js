70867b4339cdb43342102c5fcbc8d5c91b785e8e
RoeyApel <roiapel123@gmail.com>
2025-01-14 10:57:50 +0200

    f

--- Practice/Front End/Pain/script.js
+++ Practice/Front End/Pain/script.js
@@ -7,12 +7,13 @@ let flyingButton = document.querySelector("#flying-btn");
 let initialLeft = getComputedStyle(flyingButton).left;
 let initialTop = getComputedStyle(flyingButton).top;
 
+let flyingButtonTxt = flyingButton.textContent;
 let timeOut = false;
+let canFly = true;
 
 flyingButton.addEventListener("mouseover", (e) => {
-  if (timeOut) {
-    return;
-  }
+  if (!canFly || timeOut) return;
+
   timeOut = true;
   setTimeout(() => {
     timeOut = false;
@@ -21,24 +22,12 @@ flyingButton.addEventListener("mouseover", (e) => {
   initialLeft = parseInt(getComputedStyle(flyingButton).left, 10);
   initialTop = parseInt(getComputedStyle(flyingButton).top, 10);
 
-  let rndXJump = getRndNum(140, 400);
-  let rndYJump = getRndNum(140, 400);
-  let rndXDir = getRndDir();
-  let rndYDir = getRndDir();
-  let newLeft = initialLeft + rndXJump * rndXDir;
-  let newTop = initialTop + rndYJump * rndYDir;
-
-  if (newLeft < EDGES_MARGIN || newLeft > WIDTH - EDGES_MARGIN) {
-    newLeft = initialLeft + rndXJump * rndXDir * -1;
-  }
-  if (newTop < EDGES_MARGIN || newTop > HEIGHT - EDGES_MARGIN) {
-    newTop = initialTop + rndYJump * rndYDir * -1;
-  }
+  let { newLeft, newTop } = getRndPosition();
 
   let currentLeft = initialLeft;
   let currentTop = initialTop;
 
-  const interval = 15;
+  const interval = 12;
   let intervalID = setInterval(() => {
     currentLeft += (newLeft - initialLeft) / interval;
     currentTop += (newTop - initialTop) / interval;
@@ -54,6 +43,42 @@ flyingButton.addEventListener("mouseover", (e) => {
     }
   }, interval);
 });
+
+flyingButton.addEventListener("mousedown", function () {
+  canFly = false;
+  flyingButton.textContent = "X _ X";
+
+  const header = document.getElementById("header");
+
+  header.classList.add("animateTyping");
+
+  header.addEventListener(
+    "animationend",
+    function () {
+      flyingButton.textContent = flyingButtonTxt;
+      header.classList.remove("animateTyping");
+      canFly = true;
+    },
+    { once: true }
+  );
+});
+
+function getRndPosition() {
+  let rndXJump = getRndNum(140, 400);
+  let rndYJump = getRndNum(140, 400);
+  let rndXDir = getRndDir();
+  let rndYDir = getRndDir();
+  let newLeft = initialLeft + rndXJump * rndXDir;
+  let newTop = initialTop + rndYJump * rndYDir;
+
+  if (newLeft < EDGES_MARGIN || newLeft > WIDTH - EDGES_MARGIN) {
+    newLeft = initialLeft + rndXJump * rndXDir * -1;
+  }
+  if (newTop < EDGES_MARGIN || newTop > HEIGHT - EDGES_MARGIN) {
+    newTop = initialTop + rndYJump * rndYDir * -1;
+  }
+  return { newLeft, newTop };
+}
 
 function limit(num, min, max) {
   num = Math.max(min, num);
