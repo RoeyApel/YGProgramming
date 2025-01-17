5f051a31f99c7387e1ee63e89c010c6593c31728
RoeyApel <roiapel123@gmail.com>
2025-01-18 00:43:31 +0200

    hang me (not right now!)

--- Practice/Front End/Hangman/script.js
+++ Practice/Front End/Hangman/script.js
@@ -9,7 +9,9 @@ let secretWord;
 let letters = [];
 let isHangmaning = false;
 let currentHangmanPhase;
-let correctLettersTyped = new Set();
+let lettersTyped = new Set();
+let correctLetterCount;
+
 const colors = [
   "#FF6347", // Tomato
   "#FF4500", // Orange Red
@@ -54,6 +56,18 @@ const blueColors = [
   "#2F56B2", // Dark Sky Blue (Medium)
   "#3E2B97", // Dark Violet Blue
 ];
+const redColors = [
+  "#8B0000", // Dark Red
+  "#B22222", // Firebrick Red
+  "#C04E47", // Dark Scarlet Red
+  "#A52A2A", // Brown Red
+  "#800000", // Maroon
+  "#9C1C1C", // Deep Red
+  "#5C0F0F", // Dark Burgundy
+  "#B03060", // Dark Crimson
+  "#990000", // Dark Firebrick
+  "#65000B", // Deep Maroon
+];
 
 document.addEventListener("keydown", (event) => {
   if (!isHangmaning) return;
@@ -63,9 +77,21 @@ document.addEventListener("keydown", (event) => {
   if (!isALetter(letterTyped)) return;
 
   if (isCorrectLetter(letterTyped) && isLetterNotFound(letterTyped)) {
-    placeLetter(letterTyped);
-  } else {
+    placeCorrectLetter(letterTyped);
+    if (correctLetterCount == secretWord.length) {
+      hangman.style.backgroundImage = "url('/images/9.svg')";
+      hangman.style.backgroundColor = "#ffffff";
+      hangman.style.transform = "scale(" + 3.5 + ") rotate(" + 360 + "deg)";
+      isHangmaning = false;
+    }
+  } else if (isLetterNotFound(letterTyped)) {
+    placeIncorrectLetter(letterTyped);
     changeHangmanPhase();
+    if (currentHangmanPhase == 1) {
+      hangman.style.transform = "scale(" + 3.5 + ")";
+      hangman.style.backgroundColor = "#ffffff";
+      isHangmaning = false;
+    }
   }
 });
 
@@ -74,29 +100,72 @@ function isCorrectLetter(letterTyped) {
 }
 
 function isLetterNotFound(letterTyped) {
-  return correctLettersTyped.has(letterTyped) == false;
+  return lettersTyped.has(letterTyped) == false;
 }
 
-function placeLetter(letterTyped) {
-  correctLettersTyped.add(letterTyped);
+function placeCorrectLetter(letterTyped) {
+  lettersTyped.add(letterTyped);
 
   for (let i = 0; i < secretWord.length; i++) {
     if (secretWord[i] == letterTyped) {
-      let color = getRndColor();
+      let color = getRndBlueColor();
       letters[i].textContent = letterTyped;
       letters[i].style.color = color;
       letters[i].style.borderBottomColor = color;
+      correctLetterCount++;
     }
   }
 }
 
+function placeIncorrectLetter(letterTyped) {
+  lettersTyped.add(letterTyped);
+
+  let color = getRndRedColor();
+  const incorrectLetter = document.createElement("div");
+  incorrectLetter.textContent = letterTyped;
+  incorrectLetter.classList.add("incorrect-letter");
+  incorrectLetter.style.transform = "rotate(" + getRndAngel() + "deg)";
+  incorrectLetter.style.left = getRndXPresentage() + "%";
+  incorrectLetter.style.top = getRndYPresentage() + "%";
+  incorrectLetter.style.fontSize = getRndSize() + "rem";
+  incorrectLetter.style.color = color;
+  document.body.appendChild(incorrectLetter);
+}
+
+function getRndSize() {
+  let min = 1.5,
+    max = 4.5;
+
+  return Math.random() * (max - min + 1) + min;
+}
+
+function getRndXPresentage() {
+  let min1 = 5,
+    min2 = 75;
+  let max1 = 25,
+    max2 = 95;
+
+  let rnd = Math.round(Math.random());
+
+  if (rnd == 0) return Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
+  return Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
+}
+
+function getRndYPresentage() {
+  let min = 20,
+    max = 80;
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function changeHangmanPhase() {
-  currentHangmanPhase = Math.max(1, currentHangmanPhase - 1);
+  currentHangmanPhase--;
   hangman.style.backgroundImage = `url('/images/${currentHangmanPhase}.svg')`;
 }
 
 function startHangman() {
   secretWord = fetchRandomWord();
+  correctLetterCount = 0;
   currentHangmanPhase = 8;
   hangmanContainer.style.display = "flex";
   msg.style.display = "block";
@@ -126,6 +195,12 @@ loginInputs.forEach((input, index) => {
   });
 });
 
+loginInputs[0].addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    loginInputs[1].focus();
+  }
+});
+
 loginInputs[1].addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
     if (!isHangmaning && loginInputs[1].value.trim() != "") {
@@ -140,15 +215,27 @@ function isALetter(input) {
   return /^[a-zA-Z]+$/.test(input) && input.length == 1;
 }
 
-function getRndColor() {
+function getRndBlueColor() {
   let min = 0;
   let max = blueColors.length - 1;
   return blueColors[Math.floor(Math.random() * (max - min + 1)) + min];
 }
 
+function getRndRedColor() {
+  let min = 0;
+  let max = redColors.length - 1;
+  return redColors[Math.floor(Math.random() * (max - min + 1)) + min];
+}
+
+function getRndAngel() {
+  let min = -70;
+  let max = 70;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function fetchRandomWord() {
   let rndWord = wordList[getRandomIndex()];
-  while (rndWord.length < 5) {
+  while (rndWord.length < 5 || rndWord.length > 7) {
     rndWord = wordList[getRandomIndex()];
   }
   return rndWord;
