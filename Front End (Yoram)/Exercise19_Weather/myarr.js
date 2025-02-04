e52c8e16576b16740e2b9b0d48340e9350c788ef
RoeyApel <roiapel123@gmail.com>
2025-02-04 15:17:23 +0200

    g

---
+++ Front End (Yoram)/Exercise19_Weather/myarr.js
@@ -0,0 +1,209 @@
+var arr = [
+  {
+    timepoint: 3,
+    cloudcover: 7,
+    wind10m: {
+      direction: "SE",
+      speed: 2,
+    },
+    temp2m: 33,
+  },
+  {
+    timepoint: 6,
+    cloudcover: 8,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 30,
+  },
+  {
+    timepoint: 9,
+    cloudcover: 7,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 28,
+  },
+  {
+    timepoint: 12,
+    cloudcover: 4,
+    wind10m: {
+      direction: "SW",
+      speed: 2,
+    },
+    temp2m: 26,
+  },
+  {
+    timepoint: 15,
+    cloudcover: 5,
+    wind10m: {
+      direction: "W",
+      speed: 2,
+    },
+    temp2m: 25,
+  },
+  {
+    timepoint: 18,
+    cloudcover: 4,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 24,
+  },
+  {
+    timepoint: 21,
+    cloudcover: 5,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 30,
+  },
+  {
+    timepoint: 24,
+    cloudcover: 5,
+    wind10m: {
+      direction: "SE",
+      speed: 2,
+    },
+    temp2m: 34,
+  },
+  {
+    timepoint: 27,
+    cloudcover: 3,
+    wind10m: {
+      direction: "SE",
+      speed: 2,
+    },
+    temp2m: 35,
+  },
+  {
+    timepoint: 30,
+    cloudcover: 4,
+    wind10m: {
+      direction: "SW",
+      speed: 3,
+    },
+    temp2m: 31,
+  },
+  {
+    timepoint: 33,
+    cloudcover: 9,
+    wind10m: {
+      direction: "S",
+      speed: 3,
+    },
+    temp2m: 29,
+  },
+  {
+    timepoint: 36,
+    cloudcover: 9,
+    wind10m: {
+      direction: "S",
+      speed: 3,
+    },
+    temp2m: 28,
+  },
+  {
+    timepoint: 39,
+    cloudcover: 4,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 27,
+  },
+  {
+    timepoint: 42,
+    cloudcover: 4,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 26,
+  },
+  {
+    timepoint: 45,
+    cloudcover: 3,
+    wind10m: {
+      direction: "SE",
+      speed: 3,
+    },
+    temp2m: 30,
+  },
+  {
+    timepoint: 48,
+    cloudcover: 3,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 34,
+  },
+  {
+    timepoint: 54,
+    cloudcover: 6,
+    wind10m: {
+      direction: "S",
+      speed: 3,
+    },
+    temp2m: 30,
+  },
+  {
+    timepoint: 57,
+    cloudcover: 6,
+    wind10m: {
+      direction: "S",
+      speed: 3,
+    },
+    temp2m: 28,
+  },
+  {
+    timepoint: 60,
+    cloudcover: 5,
+    wind10m: {
+      direction: "S",
+      speed: 3,
+    },
+    temp2m: 27,
+  },
+  {
+    timepoint: 63,
+    cloudcover: 8,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 27,
+  },
+  {
+    timepoint: 66,
+    cloudcover: 5,
+    wind10m: {
+      direction: "W",
+      speed: 2,
+    },
+    temp2m: 26,
+  },
+  {
+    timepoint: 69,
+    cloudcover: 9,
+    wind10m: {
+      direction: "S",
+      speed: 2,
+    },
+    temp2m: 30,
+  },
+  {
+    timepoint: 72,
+    cloudcover: 8,
+    wind10m: {
+      direction: "S",
+      speed: 3,
+    },
+    temp2m: 30,
+  },
+];
---
+++ Front End (Yoram)/Exercise19_Weather/script.js
@@ -0,0 +1,7 @@
+const containerElement = document.createElement("div");
+
+function createCard(timepoint, cloudcover, direction, speed, temp2m) {
+  const cardElement = document.createElement("div");
+
+  const windElement = document.createElement("div");
+}
