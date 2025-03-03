801888c2b414f2dace51c8eecc4f42f69b5ef450
RoeyApel <137724850+RoeyApel@users.noreply.github.com>
2025-03-03 13:46:16 +0200

    Update RoeyScript.ps1

--- scripts/RoeyScript.ps1
+++ scripts/RoeyScript.ps1
@@ -2,7 +2,7 @@ Clear-Host
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install obsidian github-desktop -y
+choco install github-desktop obsidian -y
 
 $openjdk =
 @{
